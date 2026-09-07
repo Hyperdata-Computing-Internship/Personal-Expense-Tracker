dd4eb1b056321dd7844aa09714c0c621767c1a37
Syed Zaigham Hussain Shah <zaigham.shah2004@gmail.com>
2026-09-07 15:59:23 +0500

    form validation and submission event handler added

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-let expensetype = document.getElementById("exp_typ")
-let  
+let click = document.getElementById("expense-form").querySelector("button")
+
+let expense = new Object()
+
+click.addEventListener("click",function(){
+    let expensename = document.getElementById("expense-name").value
+    let expenseamount = document.getElementById("expense-amount").value
+    let expensecategory = document.getElementById("expense-category").value
+    let expensedate = document.getElementById("expense-date").value
+    if(expensename!=null || expensename.value!=undefined || expensename.value!=""){
+        expense["expense_name"]=expensename
+    }
+    if(expenseamount!=null || expenseamount!=undefined || expenseamount!=""){
+        expense["amount"]=expenseamount
+    }
+    if(expensecategory!=null || expensecategory!=undefined || expensecategory!=""){
+        expense["category"]=expensecategory
+    }
+    if(expensedate!=null || expensedate!=undefined || expensedate!=""){
+        expense["date"]=expensedate
+    }
+    print()
+})
+
+function print(){
+    console.log(expense)
+}
